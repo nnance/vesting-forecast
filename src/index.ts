303425a75f46adb8b877b2382f21ce91654daea4
Nick Nance <nance.nick@gmail.com>
2022-07-23 19:05:30 -0700

    loads the csv file and parses the lines and cells

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log('Hello World!')
+import { readFileSync } from 'fs'
+import { resolve } from 'path'
+
+const args = process.argv.slice(2)
+const file = resolve(args[0])
+const content = readFileSync(file, 'utf8')
+const lines = content.split('\r\n')
+const vesting = lines.filter((line) => line.includes('Vest Schedule'))
+const cells = vesting.map((line) => line.split(','))
+const vestingSchedule = cells.map((cell) =>
+    cell.map((value) => {
+        if (value.includes('/')) {
+            return Date.parse(value)
+        } else if (value.includes('-')) {
+            return Date.parse(value)
+        } else if (!isNaN(Number(value))) {
+            return Number(value)
+        } else {
+            return value
+        }
+    })
+)
+console.log(vestingSchedule)
