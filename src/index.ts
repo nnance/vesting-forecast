f5b8316945bbe2c0974de380a6f80b7285fd75fe
Nick Nance <nance.nick@gmail.com>
2022-07-24 09:57:59 -0700

    now reads from stdin for input

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-import { readFileSync } from 'fs'
-import { resolve } from 'path'
 import moment from 'moment'
 
 type VestEvent = {
@@ -18,60 +16,39 @@ type Grant = {
     vestEvents: VestEvent[]
 }
 
-const args = process.argv.slice(2)
-const file = resolve(args[0])
-const content = readFileSync(file, 'utf8')
-const lines = content.split('\r\n')
-
-const grants = lines.filter((line) => line.startsWith('Grant'))
-const grantSchedule: Grant[] = grants
-    .map((line) => line.split(','))
-    .map((row) => ({
-        grantNumber: row[12].includes('"') ? row[13] : row[12],
-        grantDate: new Date(row[2]),
-        grantQty: parseInt(row[4]),
-        vestedQty: parseInt(row[6]),
-        unvestedQty: parseInt(row[7]),
-        vestEvents: [],
-    }))
-
-const vesting = lines.filter((line) => line.includes('Vest Schedule'))
-const vestingSchedule: VestEvent[] = vesting
-    .map((line) => line.split(','))
-    .map((row) => ({
-        grantNumber: row[11],
-        vestPeriod: parseInt(row[18]),
-        vestDate: new Date(row[19]),
-        vestedQty: parseInt(row[25]),
-    }))
-
-const grantScheduleByGrantNumber = grantSchedule.map((grant) => ({
-    ...grant,
-    vestEvents: vestingSchedule.filter(
-        (vest) => vest.grantNumber === grant.grantNumber
-    ),
-}))
-
-const forecast: Grant[] = grantScheduleByGrantNumber.map((grant) => {
-    const { vestEvents, grantQty, grantDate } = grant
-    const periods = vestEvents.length
-    const hasCliff = grantDate > new Date('2021-01-01')
-
-    const calcQty = (period: number) =>
-        hasCliff
-            ? period === 0
-                ? Math.round(grantQty / 4)
-                : Math.round((grantQty - grantQty / 4) / (periods - 1))
-            : Math.round(grantQty / periods)
-
-    return {
-        ...grant,
-        vestEvents: vestEvents.map((vest, idx) => ({
-            ...vest,
-            vestedQty: vest.vestedQty === 0 ? calcQty(idx) : vest.vestedQty,
-        })),
+async function readStream(readable: NodeJS.ReadStream) {
+    let result = ''
+    for await (const chunk of readable) {
+        result += chunk
     }
-})
+    return result
+}
+
+function readGrants(lines: string[]): Grant[] {
+    return lines
+        .filter((line) => line.startsWith('Grant'))
+        .map((line) => line.split(','))
+        .map((row) => ({
+            grantNumber: row[12].includes('"') ? row[13] : row[12],
+            grantDate: new Date(row[2]),
+            grantQty: parseInt(row[4]),
+            vestedQty: parseInt(row[6]),
+            unvestedQty: parseInt(row[7]),
+            vestEvents: [],
+        }))
+}
+
+function readVests(lines: string[]): VestEvent[] {
+    return lines
+        .filter((line) => line.startsWith('Vest'))
+        .map((line) => line.split(','))
+        .map((row) => ({
+            grantNumber: row[11],
+            vestPeriod: parseInt(row[18]),
+            vestDate: new Date(row[19]),
+            vestedQty: parseInt(row[25]),
+        }))
+}
 
 const validateForecast = (forecast: Grant[]) => {
     const errors: string[] = []
@@ -93,39 +70,79 @@ const validateForecast = (forecast: Grant[]) => {
     }
 }
 
-// create a single array of all vest events ordered by vest date
-const vestsByDate: VestEvent[] = forecast
-    .reduce(
-        (vests, grant) => [...vests, ...grant.vestEvents],
-        [] as VestEvent[]
-    )
-    .sort((a, b) => a.vestDate.getTime() - b.vestDate.getTime())
+async function main() {
+    const content = await readStream(process.stdin)
+    const lines = content.split('\r\n')
 
-const vestsTotalByDate = vestsByDate.reduce(
-    (total, vest) => ({
-        ...total,
-        [vest.vestDate.toISOString().split('T')[0]]:
-            (total[vest.vestDate.toISOString().split('T')[0]] || 0) +
-            vest.vestedQty,
-    }),
-    {} as { [date: string]: number }
-)
+    const grantSchedule = readGrants(lines)
+    const vestingSchedule = readVests(lines)
 
-const vestsTotalByQuarter = Object.keys(vestsTotalByDate).reduce(
-    (total, date) => {
-        const quarterStr = `${moment(date).year()}-Q${moment(date).quarter()}`
+    const grantScheduleByGrantNumber = grantSchedule.map((grant) => ({
+        ...grant,
+        vestEvents: vestingSchedule.filter(
+            (vest) => vest.grantNumber === grant.grantNumber
+        ),
+    }))
+
+    const forecast: Grant[] = grantScheduleByGrantNumber.map((grant) => {
+        const { vestEvents, grantQty, grantDate } = grant
+        const periods = vestEvents.length
+        const hasCliff = grantDate > new Date('2021-01-01')
+
+        const calcQty = (period: number) =>
+            hasCliff
+                ? period === 0
+                    ? Math.round(grantQty / 4)
+                    : Math.round((grantQty - grantQty / 4) / (periods - 1))
+                : Math.round(grantQty / periods)
+
         return {
-            ...total,
-            [quarterStr]: (total[quarterStr] || 0) + vestsTotalByDate[date],
+            ...grant,
+            vestEvents: vestEvents.map((vest, idx) => ({
+                ...vest,
+                vestedQty: vest.vestedQty === 0 ? calcQty(idx) : vest.vestedQty,
+            })),
         }
-    },
-    {} as { [quarter: string]: number }
-)
+    })
 
-const vestsToCSV = Object.keys(vestsTotalByQuarter).reduce(
-    (csv, quarter) => `${csv}\n${quarter},${vestsTotalByQuarter[quarter]}`,
-    ''
-)
+    // create a single array of all vest events ordered by vest date
+    const vestsByDate: VestEvent[] = forecast
+        .reduce(
+            (vests, grant) => [...vests, ...grant.vestEvents],
+            [] as VestEvent[]
+        )
+        .sort((a, b) => a.vestDate.getTime() - b.vestDate.getTime())
 
-validateForecast(forecast)
-process.stdout.write(vestsToCSV)
+    const vestsTotalByDate = vestsByDate.reduce(
+        (total, vest) => ({
+            ...total,
+            [vest.vestDate.toISOString().split('T')[0]]:
+                (total[vest.vestDate.toISOString().split('T')[0]] || 0) +
+                vest.vestedQty,
+        }),
+        {} as { [date: string]: number }
+    )
+
+    const vestsTotalByQuarter = Object.keys(vestsTotalByDate).reduce(
+        (total, date) => {
+            const quarterStr = `${moment(date).year()}-Q${moment(
+                date
+            ).quarter()}`
+            return {
+                ...total,
+                [quarterStr]: (total[quarterStr] || 0) + vestsTotalByDate[date],
+            }
+        },
+        {} as { [quarter: string]: number }
+    )
+
+    const vestsToCSV = Object.keys(vestsTotalByQuarter).reduce(
+        (csv, quarter) => `${csv}\n${quarter},${vestsTotalByQuarter[quarter]}`,
+        ''
+    )
+
+    validateForecast(forecast)
+    process.stdout.write(vestsToCSV)
+}
+
+main()
