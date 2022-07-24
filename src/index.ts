12500329cc496672ea72a34d3b2ecd21d0ca137d
Nick Nance <nance.nick@gmail.com>
2022-07-23 22:33:06 -0700

    transform rows and columns into grants and vest

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,56 @@
 import { readFileSync } from 'fs'
 import { resolve } from 'path'
+import moment from 'moment'
+
+type VestEvent = {
+    grantNumber: string
+    vestPeriod: number
+    vestDate: Date
+    vestedQty: number
+}
+
+type Grant = {
+    grantNumber: string
+    grantDate: Date
+    grantQty: number
+    vestedQty: number
+    unvestedQty: number
+    vestEvents: VestEvent[]
+}
 
 const args = process.argv.slice(2)
 const file = resolve(args[0])
 const content = readFileSync(file, 'utf8')
 const lines = content.split('\r\n')
+
+const grants = lines.filter((line) => line.startsWith('Grant'))
+const grantSchedule: Grant[] = grants
+    .map((line) => line.split(','))
+    .map((row) => ({
+        grantNumber: row[12],
+        grantDate: new Date(row[2]),
+        grantQty: parseInt(row[4]),
+        vestedQty: parseInt(row[6]),
+        unvestedQty: parseInt(row[7]),
+        vestEvents: [],
+    }))
+
 const vesting = lines.filter((line) => line.includes('Vest Schedule'))
-const cells = vesting.map((line) => line.split(','))
-const vestingSchedule = cells.map((cell) =>
-    cell.map((value) => {
-        if (value.includes('/')) {
-            return Date.parse(value)
-        } else if (value.includes('-')) {
-            return Date.parse(value)
-        } else if (!isNaN(Number(value))) {
-            return Number(value)
-        } else {
-            return value
-        }
-    })
-)
-console.log(vestingSchedule)
+const vestingSchedule: VestEvent[] = vesting
+    .map((line) => line.split(','))
+    .map((row) => ({
+        grantNumber: row[11],
+        vestPeriod: parseInt(row[18]),
+        vestDate: new Date(row[19]),
+        vestedQty: parseInt(row[25]),
+    }))
+
+const grantScheduleByGrantNumber = vestingSchedule.reduce((grants, vest) => {
+    const grant = grants.find((g) => g.grantNumber === vest.grantNumber)
+    if (grant) {
+        grant.vestEvents.push(vest)
+    }
+    return grants
+}, grantSchedule)
+
+console.dir(grantSchedule, { depth: null })
